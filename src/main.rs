4acb289217dd28ea07b2b855a1806188fa607cbb
Behxhet Sadiku <bensadiku65@gmail.com>
2021-02-17 16:10:59 +0100

    Allow base urls to change, refactored some events, added some tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use azure_rs::pull_requests::{PullListOptions, PullUpdateOptions};
-use azure_rs::{ApiVersion, AzureClient, Credentials, Result};
+use azure_rs::pull_requests::{PullListOptions, PullRequestResponse, PullUpdateOptions};
+use azure_rs::{ApiVersion, AzureClient, Credentials, Future, Result};
 use std::env::var;
 use std::fs;
 use std::process::Command;
@@ -11,13 +11,9 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{BufWriter, Read, Write};
 fn main() -> Result<()> {
-    init_clap();
     println!("Hello, world!");
-    println!("get_upstream: {:?}", get_upstream());
-    println!("current_branch: {:?}", get_current_branch());
-    println!("get_last_commit {:?}", get_last_commit());
+    init_clap();
 
-    println!("getting now");
     let _ = get();
 
     Ok(())
@@ -89,48 +85,68 @@ pub fn get_cfg_home() -> String {
 pub async fn get() -> Result<()> {
     let remote = get_upstream();
 
-    // let remote = "https://tfs.company.com:22/Company/MyProject/_git/Project.Rust.App";
-
-    let list: Vec<&str> = remote.split("/").collect();
-    let repo_name: &str = list.last().expect("Could not find repo name");
-    let project_name: &str = list[4];
-    let org_name: &str = list[3];
+    let (org_name, project_name, repo_name) = get_details(remote);
     let branch = get_current_branch();
     println!(
         "Repo: {:?} Project: {:?} Org: {:?}",
         repo_name, project_name, org_name
     );
 
-    let client = get_client(org_name);
+    let client = get_client(&org_name);
     let options = PullListOptions::builder()
         .source_ref_name(format!("refs/heads/{}", branch))
         .build();
+
+    // Only one pull request per branch, even though this is an array, there should be only 1 pull requst or none
     let pr = client
-        .repo(project_name, repo_name)
+        .repo(&project_name, &repo_name)
         .pulls()
         .list(options)
         .await?;
-    println!("Got pull request");
 
-    let draft_options = PullUpdateOptions::builder().draft(true).build();
+    if pr.count > 0 {
+        let pull = &pr.value[0];
+        let pull_id = pull.pull_request_id;
+        let is_draft = pull.is_draft;
+        println!("Got pull request: id={} draft={}", pull_id, is_draft);
 
-    let _ = client
-        .repo(project_name, repo_name)
-        .pull(pr.value[0].pull_request_id)
-        .update(&draft_options)
-        .await;
-
-    println!("Set pull request to draft");
-
-    let draft_options = PullUpdateOptions::builder().draft(false).build();
-    let _ = client
-        .repo(project_name, repo_name)
-        .pull(pr.value[0].pull_request_id)
-        .update(&draft_options)
-        .await;
-    println!("Removed pull request from draft");
+        // If it's already draft, publish right away
+        if !is_draft {
+            let draft_options = PullUpdateOptions::builder().draft(true).build();
+            let _ = client
+                .repo(&project_name, &repo_name)
+                .pull(pull_id)
+                .update(&draft_options)
+                .await;
+        }
+        let draft_options = PullUpdateOptions::builder().draft(false).build();
+        let _ = client
+            .repo(project_name, repo_name)
+            .pull(pull_id)
+            .update(&draft_options)
+            .await;
+    } else {
+        println!("No pull request found for {:?}", branch);
+    }
 
     Ok(())
+}
+
+pub async fn update_draft(
+    client: &AzureClient,
+    pull_id: u64,
+    is_draft: bool,
+) -> Future<PullRequestResponse> {
+    let remote = get_upstream();
+
+    let (_, project_name, repo_name) = get_details(remote);
+    let draft_options = PullUpdateOptions::builder().draft(is_draft).build();
+    println!("{},{}", project_name, repo_name);
+    let response = client
+        .repo(project_name, repo_name)
+        .pull(pull_id)
+        .update(&draft_options);
+    response
 }
 
 // git config --get remote.origin.url
@@ -146,17 +162,6 @@ pub fn get_upstream() -> String {
     msg.into()
 }
 
-// git rev-parse HEAD
-pub fn get_last_commit() -> String {
-    let output = Command::new("git")
-        .arg("rev-parse")
-        .arg("HEAD")
-        .output()
-        .expect("No remote url found ?");
-    let msg = String::from_utf8_lossy(&output.stdout);
-    msg.into()
-}
-
 // git rev-parse --abbrev-ref HEAD
 pub fn get_current_branch() -> String {
     let output = Command::new("git")
@@ -164,13 +169,42 @@ pub fn get_current_branch() -> String {
         .arg("--abbrev-ref")
         .arg("HEAD")
         .output()
-        .expect("No remote url found ?");
+        .expect("No current branch ?");
     let msg = String::from_utf8_lossy(&output.stdout);
     let mut branch: String = msg.into();
     if branch.ends_with('\n') {
         branch.pop();
     }
     branch
+}
+
+// Get base url from the upstream url
+pub fn get_base_url(url: String) -> String {
+    let mut base_url: String = url;
+
+    // replace ssh
+    if base_url.starts_with("ssh") {
+        base_url = base_url.replace("ssh", "https");
+    }
+    // start looping after https://, it's a base url if next character is : or /
+    for x in 8..base_url.len() {
+        let character = base_url.chars().nth(x).unwrap();
+        if character == ':' || character == '/' {
+            let (first, _) = base_url.split_at(x);
+            base_url = first.to_string();
+            break;
+        }
+    }
+    base_url
+}
+
+// Returns the organization, project and repository we are currently in
+pub fn get_details(remote: String) -> (String, String, String) {
+    let list: Vec<&str> = remote.split("/").collect();
+    let repo_name: &str = list.last().expect("Could not find repo name");
+    let project_name: &str = list[4];
+    let org_name: &str = list[3];
+    (org_name.into(), project_name.into(), repo_name.into())
 }
 
 /// Returns a simple client to the other examples
@@ -184,5 +218,44 @@ pub fn get_client(org: &str) -> AzureClient {
     // Client defaults to this anyway
     azure.set_api_version(ApiVersion::V5_1);
 
+    // Set base host for the client based on the git remote upstream
+    // if not set, it defaults to dev.azure.com
+    let url = get_upstream();
+    let base_url = get_base_url(url);
+    azure.set_host(base_url);
+
     azure
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{get_base_url, get_details};
+    #[test]
+    fn parse_https_base_url() {
+        let url = "https://test.draftpush.com/Company/MyProject/_git/Project.Rust.App".into();
+        let base = get_base_url(url);
+        assert_eq!(base, "https://test.draftpush.com");
+    }
+    #[test]
+    fn parse_ssh_base_url() {
+        let url = "ssh://test.draftpush.com/Company/MyProject/_git/Project.Rust.App".into();
+        let base = get_base_url(url);
+        assert_eq!(base, "https://test.draftpush.com");
+    }
+    #[test]
+    fn parse_ssh_port_base_url() {
+        let url = "ssh://test.draftpush.com:44/Company/MyProject/_git/Project.Rust.App".into();
+        let base = get_base_url(url);
+        assert_eq!(base, "https://test.draftpush.com");
+    }
+
+    #[test]
+    fn parse_details() {
+        let url = "https://test.draftpush.com/Company/MyProject/_git/Project.Rust.App".into();
+        let (org_name, project_name, repo_name) = get_details(url);
+
+        assert_eq!(org_name, "Company");
+        assert_eq!(project_name, "MyProject");
+        assert_eq!(repo_name, "Project.Rust.App");
+    }
 }
